ab80bcf60bd09fc017d41037c23f2261b1fa3141
Suleman Rasheed <bcsf17m040@pucit.edu.pk>
2020-09-08 18:53:43 +0500

    Created NavBar

---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from './styles.module.css';
+import { Link } from 'react-router-dom';
+export const Nav = () => {
+    const {
+        navLinks,
+        logo,
+        navBar,
+        navLink
+    } = styles
+    return (
+        <nav className={navBar}>
+            <div className={logo}>SPACE X</div>
+            <ul className={navLinks}>
+                <li className={navLink}>
+                    <Link to='/'>
+                        Home
+                    </Link>
+                </li>
+                <li className={navLink}>
+                    <Link to='/launches'>
+                        Launches
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
