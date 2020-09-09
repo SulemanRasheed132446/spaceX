51c0a4533bb3b1e9d3c12db3dc979a40d54e273c
Suleman Rasheed <bcsf17m040@pucit.edu.pk>
2020-09-09 12:52:31 +0500

    Service Worker Added

---
+++ src/sw.js
@@ -0,0 +1,21 @@
+const cacheName = "GAPHQL_APP";
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(
+                [
+                    'https://spacexdata.herokuapp.com/graphql'
+                ]
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request);
+        })
+    );
+});
+
